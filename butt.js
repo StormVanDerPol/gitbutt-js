d1e69bb0c2c64b3436fa6106a6abcb0ee8038232
Storminya <psostorm@gmail.com>
2021-12-16 10:21:59 +0100

    12/16/2021, 10:21:59 AM

--- butt.js
+++ butt.js
@@ -2,16 +2,16 @@ const path = require("path");
 const fs = require("fs");
 const { execSync } = require("child_process");
 const buttPath = path.join(__dirname, "butt.js");
-const message = `// butt time: ${new Date().toLocaleString()}`;
+const message = new Date().toLocaleString();
 const butt = fs.readFileSync(buttPath);
 const lines = butt
   .toString()
   .split("\n")
   .filter((l) => l);
-lines.reverse().splice(0, 1, message);
+lines.reverse().splice(0, 1, `/* ${message} */`);
 lines.reverse();
 const doc = lines.join("\n");
 fs.writeFileSync(buttPath, doc);
 execSync(`git add . && git commit -m "${message}"`);
 execSync("git push origin master");
-// butt time: 12/15/2021, 1:00:01 PM
+/* 12/16/2021, 10:21:59 AM */
