b9b8df349f8b1fe36890bef4b90adfb68314be31
Storminya <psostorm@gmail.com>
2021-12-19 13:00:01 +0100

    12/19/2021, 1:00:01 PM

--- butt.js
+++ butt.js
@@ -14,4 +14,4 @@ const doc = lines.join("\n");
 fs.writeFileSync(buttPath, doc);
 execSync(`git add . && git commit -m "${message}"`);
 execSync("git push origin master");
-/* 12/18/2021, 1:00:01 PM */
+/* 12/19/2021, 1:00:01 PM */
