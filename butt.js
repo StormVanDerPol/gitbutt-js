e8ad572b5507c5603157f3d34c62bfa072d964a8
Storminya <psostorm@gmail.com>
2022-01-04 13:00:01 +0100

    1/4/2022, 1:00:01 PM

--- butt.js
+++ butt.js
@@ -14,4 +14,4 @@ const doc = lines.join("\n");
 fs.writeFileSync(buttPath, doc);
 execSync(`git add . && git commit -m "${message}"`);
 execSync("git push origin master");
-/* 1/3/2022, 1:00:01 PM */
+/* 1/4/2022, 1:00:01 PM */
