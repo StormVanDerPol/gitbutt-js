71aa485ac80d07a5d92124542e135cf23eafeaf9
StormVanDerPol <psostorm@gmail.com>
2022-03-26 09:43:02 +0100

    26/03/2022, 09:43:02

--- butt.js
+++ butt.js
@@ -14,4 +14,4 @@ const doc = lines.join("\n");
 fs.writeFileSync(buttPath, doc);
 execSync(`git add . && git commit -m "${message}"`);
 execSync("git push origin master");
-/* 26/03/2022, 09:42:01 */
+/* 26/03/2022, 09:43:02 */
