b1f8964edc06d2eb3e78d14c5ecd573abca2879c
StormVanDerPol <psostorm@gmail.com>
2022-04-19 11:08:18 +0200

    2022-04-19T09:08:18.715Z

--- butt.js
+++ butt.js
@@ -2,22 +2,29 @@ const path = require("path");
 const fs = require("fs");
 const { execSync } = require("child_process");
 const buttPath = path.join(__dirname, "butt.js");
-const message = new Date().toLocaleString();
 const butt = fs.readFileSync(buttPath);
 const lines = butt
   .toString()
   .split("\n")
   .filter((l) => l);
-lines.reverse().splice(0, 1, `/* ${message} */`);
+lines.reverse();
+const then = new Date(lines[0].trim());
+const now = new Date();
+if (then.getDate() === now.getDate()) {
+  console.log("Already did a butt today")
+  process.exit(0);
+}
+const message = now.toISOString();
+lines.splice(0, 1, `/* ${message} */`);
 lines.reverse();
 const doc = lines.join("\n");
 fs.writeFileSync(buttPath, doc);
-const retry = (fn, delay) => {
+const retry = (fn, maxtries, delay) => {
   try {
     fn();
   } catch (err) {
     if (maxTries <= 0) throw err;
-    setTimeout(() => retry(fn, delay), delay);
+    setTimeout(() => retry(fn, maxtries, delay), delay);
   }
 };
 retry(
@@ -25,6 +32,7 @@ retry(
     execSync(`git add . && git commit -m "${message}"`);
     execSync("git push origin master");
   },
+  100,
   5000
 );
-/* 19/04/2022, 07:33:36 */
+/* 2022-04-19T09:08:18.715Z */
